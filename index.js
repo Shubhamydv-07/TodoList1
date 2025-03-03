38438c83a199bd7bd01c50a238f50cf93f7f172e
Shubhamydv-07 <144944097+Shubhamydv-07@users.noreply.github.com>
2025-03-03 19:50:29 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,52 @@
+// document.querySelector("form").addEventListener("submit",myTodo);
+// function myTodo(e){
+//     e.preventDefault();
+//     let taskName = document.querySelector("#task").value;
+//     let taskPriority = document.querySelector("#priority").value;
+    
+//     displayTable(taskName,taskPriority);
+// }
+// function displayTable(taskName , taskPriority){
+//     let row = document.createElement("tr");
+//     let td1 = document.createElement("td");
+//     td1.innerText = taskName;
+//     let td2 = document.createElement("td");
+//     td2.innerText = taskPriority;
+//     if(taskPriority == "High")td2.style.backgroundColor = "red";
+//     else td2.style.backgroundColor = "yellow";
+//     let td3 = document.createElement("td");
+//     td3.innerText = "Add to Fav ";
+//     row.append(td1,td2,td3);
+//     document.querySelector("tbody").append(row);
+// }
+
+// document.querySelector("form").addEventListener("submit",myTodo);
+// function myTodo(e){
+//     e.preventDefault();
+//     let taskName = document.querySelector("#task").value;
+//     let taskPriority = document.querySelector("#priority").value;
+    
+//     let taskobj =  {taskName  , taskPriority};
+//     console.log(taskobj);
+//     // displayTable(taskName,taskPriority);
+//     displayTable(taskobj);
+// }
+// function displayTable(taskobj){
+//     let row = document.createElement("tr");
+//     let td1 = document.createElement("td");
+//     td1.innerText = taskobj.taskName;
+//     let td2 = document.createElement("td");
+//     td2.innerText = taskobj.taskPriority;
+//     if(taskobj.taskPriority == "High")td2.style.backgroundColor = "red";
+//     else td2.style.backgroundColor = "yellow";
+//     let td3 = document.createElement("td");
+//     td3.innerText = "Add to Fav ";
+//     row.append(td1,td2,td3);
+//     document.querySelector("tbody").append(row);
+// }
+
+// displayTable(todoArr);
+
 document.querySelector("form").addEventListener("submit",myTodo);
 let todoArr = JSON.parse(localStorage.getItem("todo")) || [];
 let favArr = JSON.parse(localStorage.getItem("favArr")) || [];
